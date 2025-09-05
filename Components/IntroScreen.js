488af160d459473ff933d72d2a39a955609064c2
Rani Singh <singhrani6792@gmail.com>
2025-09-05 19:14:23 +0530

    intro page is complete

---
+++ Components/IntroScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const IntroScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/ccl_logo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.textUpper}>CCL</Text>
+      <Text style={styles.textNormal}>IIT Gandhinagar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4682B4',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+  },
+  textUpper: {
+    fontFamily: 'serif',   
+    fontSize: 24,
+    fontWeight: '400',
+    textAlign: 'center',
+    color: '#fff',
+    textTransform: 'uppercase',
+  },
+  textNormal: {
+    fontFamily: 'serif',  
+    fontSize: 24,
+    fontWeight: '400',
+    textAlign: 'center',
+    color: '#fff',
+    marginTop: 5,
+  },
+});
+
+export default IntroScreen;
